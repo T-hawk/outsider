fc779ca5129fec232ee5bf3bfe3dcd6c9a21ef90
Kyle Grinstead <kyle@kylegrinstead.com>
2020-08-08 11:29:34 -0400

    added NoSleep and fixed indentation

--- src/SelectMaster.svelte
+++ src/SelectMaster.svelte
@@ -1,13 +1,13 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-    const dispatch = createEventDispatcher();
-    const next = () => dispatch('next')
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+  const next = () => dispatch('next')
 </script>
 
 <main>
-    <div on:click={next} class="card">
-        <h2 class="white">Choose someone to be the master and pass them the phone</h2>
-        <br>
-        <h3>Master, tap to see the word</h3>
-    </div>
+  <div on:click={next} class="card">
+    <h2 class="white">Choose someone to be the master and pass them the phone</h2>
+    <br>
+    <h3>Master, tap to see the word</h3>
+  </div>
 </main>
